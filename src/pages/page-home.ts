943671ab89868fc8e345374dc9301bcd161c4030
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-07-20 14:04:41 +0430

    feat(pages): home page component

--- src/pages/page-home.ts
+++ src/pages/page-home.ts
@@ -1,7 +1,10 @@
+import {router} from '@alwatr/router';
 import {css, html} from 'lit';
 import {customElement} from 'lit/decorators/custom-element.js';
+import {when} from 'lit/directives/when.js';
 
 import {AppElement} from '../app-debt/app-element';
+import {Tools} from '../config';
 
 import type {ListenerInterface} from '@alwatr/signal';
 import type {TemplateResult, CSSResult} from 'lit';
@@ -21,10 +24,7 @@ declare global {
  */
 @customElement('page-home')
 export class PageHome extends AppElement {
-  static override styles = [
-    ...(<CSSResult[]>AppElement.styles),
-    css``,
-  ];
+  static override styles = [...(<CSSResult[]>AppElement.styles), css``];
 
   protected _listenerList: Array<unknown> = [];
 
@@ -39,6 +39,19 @@ export class PageHome extends AppElement {
   }
 
   override render(): TemplateResult {
-    return html``;
+    return html` <ion-content> ${this._renderToolsList()} </ion-content> `;
+  }
+
+  protected _renderToolsList(): TemplateResult {
+    const toolsTemplate = Tools.map(
+        (tool) => html`
+        <ion-item href=${router.makeUrl({sectionList: [tool.id]})}>
+          ${when(tool.icon, () => html`<er-iconsax name=${tool.icon} category="broken" slot="start"></er-iconsax>`)}
+          <ion-label>${this._localize.term(tool.id)}</ion-label>
+        </ion-item>
+      `,
+    );
+
+    return html` <ion-list lines="full">${toolsTemplate}</ion-list>`;
   }
 }
