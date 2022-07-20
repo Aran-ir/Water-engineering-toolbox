06d5365bbe14b3fb5efc2ff8718de4cbe2015e0f
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-07-20 14:01:20 +0430

    feat(i18n): en translation

---
+++ src/translation/en.ts
@@ -0,0 +1,16 @@
+import {DefaultTranslation} from '@shoelace-style/localize/dist/index.js';
+
+const translation: DefaultTranslation = {
+  '$code': 'en',
+  '$name': 'English',
+  '$dir': 'ltr',
+
+  'flow-rate-calculator-gunya-method': 'Flow Rate Calculator (Gunya Method)',
+  'flow-calculator': 'Flow Calculator',
+  'horizontal-distance': 'Horizontal Distance',
+  'pipe-diameter': 'Pipe Diameter (in inches)',
+  'upload-image': 'Upload Image',
+  'calculation': 'Calculation',
+};
+
+export default translation;
